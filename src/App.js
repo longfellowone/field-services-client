1433298ec96bbcc05e9864b31c1ca9cb0dce1444
Matt Wright <mattwright3195@gmail.com>
2019-01-19 11:49:10 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="">
+        <p>Hello World!</p>
+      </div>
+    );
+  }
+}
+
+export default App;
